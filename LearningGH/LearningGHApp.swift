466a4b69ba35009f487f25ce037e4c811fb0ac5b
Alex Trubacs <aleksandrs.trubacs@icloud.com>
2021-03-31 18:51:20 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Aleksandrs Trubacs on 31/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
